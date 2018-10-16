88cb70834cd215806830dffce2e299d1774bce80
maxence4283 <maxencen.contact@gmail.com>
2018-10-16 21:00:09 +0200

    Mise en place des différents répertoires, mise en place de bootstrap 4, commencement de l'index jusqu'au compétence.

---
+++ index.php
@@ -0,0 +1,140 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Site officiel de l'association 'Les enfants d'arc en ciel' vous trouverez ici différentes informations conçerant l'histoire de notre association.">
+    <meta name="keywords" content="Association, enfants, arc, en, ciel, arc-en-ciel, les villettes, 43600, France, Les enfants d'arc en ciel, Association les enfants d'arc en ciel, villettes, Gagnoa, cote-d'ivoire">
+    <meta name="author" content="NICOLE Maxence">
+
+    <link rel="stylesheet" href="Public/css/bootstrap.css">
+    <link rel="stylesheet" href="Public/css/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <script src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="Public/js/bootstrap.js"></script>
+
+    <title>Maxence NICOLE</title>
+
+  </head>
+  <body>
+
+  <div class="container-fluid">
+  	<div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-12 col-lg-12">
+  		<nav class="navbar navbar-expand-lg navbar-light bg-light2">
+
+  			<a class="navbar-brand display-4" href="#">Maxence NICOLE</a>
+
+  			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			    <span class="navbar-toggler-icon"></span>
+			 </button>
+			  <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+			    <ul class="navbar-nav">
+			      <li class="nav-item active">
+			        <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="#">Présentation</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="#">Compétences</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="#">Projets</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="#">CV</a>
+			      </li>
+			    </ul>
+			  </div>
+		</nav>
+  	</div>
+  </div>
+<div id="Presentation">
+  <div class="container-fluid">
+  	<div class="row">
+	  	<div class="col-12 col-xs-12 col-sm-12 col-md-9 col-xl-9 col-lg-9">
+	  		<p class="text-center display-2" style="margin-bottom: -2%">
+	  			WEB DEVELOPER
+	  		</p>
+                <hr class="my-5">
+            <div class="row">
+                <div class="col-12 col-xs-12 col-sm-12 col-md-9 col-xl-6 col-lg-6">
+                    <div class="" style="font-size: 1.2rem; margin-left: 2%;margin-right: 2%">
+                        NOM : NICOLE<br>
+                        PRENOM : Maxence<br>
+                        DATE DE NAISSANCE : 08/03/1998<br>
+                        LIEU DE NAISSANCE : Saint-Priest-En-Jarez.<br>
+                        NIVEAU D'ETUDE : <br>
+                        Je suis en BTS SIO (Services Informatiques aux Organisations) option SLAM (Solutions logicielles et applications métiers forme des spécialistes des logiciels).<br>
+                        ECOLE : Lycée Simone Weil (Saint-Etienne)<br>
+                        FORMATIONS : BAC STMG SIG (Mention BIEN), Permis de conduire.
+                    </div>
+                </div>
+                <div class="col-12 col-xs-12 col-sm-12 col-md-9 col-xl-6 col-lg-6">
+                    <div class="" style="font-size: 1.2rem; margin-left: 2%;margin-right: 2%">
+                        CENTRE D'INTERETS : <br>
+                        J'ai différents centres d'intérêts dans la vie quotidienne tel que le sport et plus particulièrement le foot et bien évidement l'informatique, je développe assez souvent à la maison et suit de très près les différentes évolutions dans le domaine de l'informatique et de la technologie en général. <br>
+                        OBJECTIF : <br>
+                        Mon objectif après le BTS est de pouvoir intégrer une licence proffessionnelle en alternance afin de trouver un travail me permettant d'etre pleinement épanouis dans ce que je vais faire.
+                    </div>
+                </div>
+            </div>
+	  	</div>
+	  	<div class="col-12 col-xs-12 col-sm-12 col-md-3 col-xl-3 col-lg-3">
+	  		<div class="shadow-lg p-3 mb-5 bg-white rounded" style="margin-top: 10%;margin-right: 5%">
+	  			<img src="Public/img/moi.jpg" style="max-width: 100%"/>
+	  		</div>
+	  	</div>
+	</div>
+  </div>
+</div>
+<div id="Compétence">
+  <div class="container-fluid">
+	<div class="row">
+		<div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-12 col-lg-12" style="background-color: #B7BDBF">
+			<p class="text-center display-4" style="margin-bottom: -1%;padding-top: 0.75%">
+	  			Compétence
+	  		</p>
+	  		<hr class="my-5">
+	  			<div class="row" style="margin-top: -1%">
+	  				<div class="col-12 col-xs-12 col-sm-12 col-md-6 col-xl-6 col-lg-6 border-right border-secondary">
+	  					<p class="" style="font-size: 1.2rem; margin-left: 2%;margin-right: 2%">
+				  			Je m'appelle Maxence NICOLE, J'ai 20 ans, je suis né le 08/03/1998 à Saint-Etienne et  je suis actuellement élève en BTS SIO dans le Lycée Simone Weil situé a Saint-Priest-En-Jarez.<br>
+				  			Je possède en mon actif un BAC STMG option SIG avec mention BIEN.<br>
+				  			Dans ce site Portfolio je vais vous présenter l'ensemble de mes compétences ainsi que les différents projets que j'ai effectué durant ma scolarité.<br>
+				  			Pour finir je vais vous laisser la possibilité de télécharger mon CV.
+				  		</p>
+	  				</div>
+	  				<div class="col-12 col-xs-12 col-sm-12 col-md-6 col-xl-6 col-lg-6">
+                            <p class="lead">SQL</p>
+                        <div class="progress">
+                            <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: 90%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"><div style="color:black">90%</div></div>
+                        </div><br>
+                        <p class="lead">HTML/CSS (BOOSTRAP)</p>
+                        <div class="progress">
+                            <div class="progress-bar2 progress-bar-striped progress-bar-animated" role="progressbar" style="width: 80%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"><div style="color:black">80%</div></div>
+                        </div><br>
+                        <p class="lead">PHP</p>
+                        <div class="progress">
+                            <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: 70%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"><div style="color:black">70%</div></div>
+                        </div><br>
+                        <p class="lead">LARAVEL</p>
+                        <div class="progress">
+                            <div class="progress-bar2 progress-bar-striped progress-bar-animated" role="progressbar" style="width: 45%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"><div style="color:black">45%</div></div>
+                        </div><br>
+                        <p class="lead">JAVA</p>
+                        <div class="progress">
+                            <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: 40%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"><div style="color:black">40%</div></div>
+                        </div>
+	  				</div>
+	  			</div>
+		</div>
+	</div>
+  </div>
+</div>
+
+
+
+</body>
+</html> 
