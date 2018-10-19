86b2e00e9a1548f74115148f35eb4285f1e96c53
maxence4283 <maxencen.contact@gmail.com>
2018-10-19 18:07:55 +0200

    Mise en responsive de la partie compétence

--- index.php
+++ index.php
@@ -254,11 +254,56 @@
                 <hr class="my-5">
 
                     <div class="row" style="margin-top: -1%">
-                        <div class="col-12 col-xs-12 col-sm-12 col-md-6 col-xl-6 col-lg-6 border-right border-secondary">
+                        <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-6 col-lg-6 border-right border-secondary">
 
                             <div class="shadow-lg p-3 mb-5 rounded" style="background-color: #dfe6e9; margin-right: 3%; margin-left: 3%">
 
-                                <p class="champagne" style="font-size: 1.2rem; margin-left: 2%;margin-right: 2%">
+                                <!-- partie lg-->
+                                <p class="champagne d-none d-xl-block" style="font-size: 1.2vw; margin-left: 2%;margin-right: 2%">
+                                    Mes compétences acquises lors de mon cursus scolaires et personnelles sont nombreuses :<br><br>
+                                    Je suis passionné par les bases de données et tout ce qui les entoures et nottement le langage SQL que je maîtrise pratiquement à 100% seule quelque notions me sont a apprendre. <br><br>
+                                    En ce qui concerne le HTML je n'est absolument aucun soucis et pour le CSS de nombreuses compétences me sont acquise nottement avec le framework BOOTSTRAP que je pratique beaucoup.<br><br>
+                                    Pour ce qui est du PHP je le pratique assez régulièrement mais étant donnée la richesse de ce langage je ne le maîtrise pas a 100%, de nombreuse bases du langages sont acquise.<br>
+                                    La programmation orientée objet et le modèle MVC sont partiellement acquis et en cours d'acquisition.<br><br>
+                                    Quant au framework Laravel je m'y interresse activement et suit de nombreux tutoriels le concernant. <br><br>
+                                    JAVA à été vue en première année avec la pratique du JAVA de base et du JAVA en programmation orientée objet.<br>
+                                </p>
+
+                                <!-- partie xl-->
+                                <p class="champagne d-none d-lg-block d-xl-none" style="font-size: 1.2rem; margin-left: 2%;margin-right: 2%">
+                                    Mes compétences acquises lors de mon cursus scolaires et personnelles sont nombreuses :<br><br>
+                                    Je suis passionné par les bases de données et tout ce qui les entoures et nottement le langage SQL que je maîtrise pratiquement à 100% seule quelque notions me sont a apprendre. <br><br>
+                                    En ce qui concerne le HTML je n'est absolument aucun soucis et pour le CSS de nombreuses compétences me sont acquise nottement avec le framework BOOTSTRAP que je pratique beaucoup.<br><br>
+                                    Pour ce qui est du PHP je le pratique assez régulièrement mais étant donnée la richesse de ce langage je ne le maîtrise pas a 100%, de nombreuse bases du langages sont acquise.<br>
+                                    La programmation orientée objet et le modèle MVC sont partiellement acquis et en cours d'acquisition.<br><br>
+                                    Quant au framework Laravel je m'y interresse activement et suit de nombreux tutoriels le concernant. <br><br>
+                                    JAVA à été vue en première année avec la pratique du JAVA de base et du JAVA en programmation orientée objet.<br>
+                                </p>
+
+                                <!-- partie md-->
+                                <p class="champagne d-none d-md-block d-lg-none" style="font-size: 1.2rem; margin-left: 2%;margin-right: 2%">
+                                    Mes compétences acquises lors de mon cursus scolaires et personnelles sont nombreuses :<br><br>
+                                    Je suis passionné par les bases de données et tout ce qui les entoures et nottement le langage SQL que je maîtrise pratiquement à 100% seule quelque notions me sont a apprendre. <br><br>
+                                    En ce qui concerne le HTML je n'est absolument aucun soucis et pour le CSS de nombreuses compétences me sont acquise nottement avec le framework BOOTSTRAP que je pratique beaucoup.<br><br>
+                                    Pour ce qui est du PHP je le pratique assez régulièrement mais étant donnée la richesse de ce langage je ne le maîtrise pas a 100%, de nombreuse bases du langages sont acquise.<br>
+                                    La programmation orientée objet et le modèle MVC sont partiellement acquis et en cours d'acquisition.<br><br>
+                                    Quant au framework Laravel je m'y interresse activement et suit de nombreux tutoriels le concernant. <br><br>
+                                    JAVA à été vue en première année avec la pratique du JAVA de base et du JAVA en programmation orientée objet.<br>
+                                </p>
+
+                                <!-- partie sm-->
+                                <p class="champagne d-none d-sm-block d-md-none" style="font-size: 1.2rem; margin-left: 2%;margin-right: 2%">
+                                    Mes compétences acquises lors de mon cursus scolaires et personnelles sont nombreuses :<br><br>
+                                    Je suis passionné par les bases de données et tout ce qui les entoures et nottement le langage SQL que je maîtrise pratiquement à 100% seule quelque notions me sont a apprendre. <br><br>
+                                    En ce qui concerne le HTML je n'est absolument aucun soucis et pour le CSS de nombreuses compétences me sont acquise nottement avec le framework BOOTSTRAP que je pratique beaucoup.<br><br>
+                                    Pour ce qui est du PHP je le pratique assez régulièrement mais étant donnée la richesse de ce langage je ne le maîtrise pas a 100%, de nombreuse bases du langages sont acquise.<br>
+                                    La programmation orientée objet et le modèle MVC sont partiellement acquis et en cours d'acquisition.<br><br>
+                                    Quant au framework Laravel je m'y interresse activement et suit de nombreux tutoriels le concernant. <br><br>
+                                    JAVA à été vue en première année avec la pratique du JAVA de base et du JAVA en programmation orientée objet.<br>
+                                </p>
+
+                                <!-- partie xs-->
+                                <p class="champagne d-block d-sm-none" style="font-size: 1.2rem; margin-left: 2%;margin-right: 2%">
                                     Mes compétences acquises lors de mon cursus scolaires et personnelles sont nombreuses :<br><br>
                                     Je suis passionné par les bases de données et tout ce qui les entoures et nottement le langage SQL que je maîtrise pratiquement à 100% seule quelque notions me sont a apprendre. <br><br>
                                     En ce qui concerne le HTML je n'est absolument aucun soucis et pour le CSS de nombreuses compétences me sont acquise nottement avec le framework BOOTSTRAP que je pratique beaucoup.<br><br>
@@ -271,7 +316,7 @@
                             </div>
                         </div>
 
-                        <div class="col-12 col-xs-12 col-sm-12 col-md-6 col-xl-6 col-lg-6 caviardreams">
+                        <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-6 col-lg-6 caviardreams">
 
                             <p class="lead">SQL</p>
                                 <div class="progress">
@@ -337,7 +382,7 @@
                               <p class="card-text champagne" style="margin-top: -5%">Voici un site internet que j'ai réalisé à partir d'une page blanche pour une association
                               dans le cadre de mon stage de première année. La maintenance, le lien avec l'hébérgeur et l'evolution du
                               site internet est géré par moi-même.</p>
-                              <a href="https://www.lesenfantsdarcenciel.ovh/" class="btn btn-outline-secondary">Lien du site</a>
+                              <a href="https://www.lesenfantsdarcenciel.ovh/" class="btn btn-outline-secondary" target="_blank" >Lien du site</a>
                           </div>
                       </div>
 
