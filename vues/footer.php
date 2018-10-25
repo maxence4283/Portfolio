7c57b4cad320b19fee79855050423d4d35c37c3a
maxence4283 <maxencen.contact@gmail.com>
2018-10-25 15:09:49 +0200

    Mise en place du placement de toute les colonnes du footer ajout de deux liens (Linkedin et Github)

--- vues/footer.php
+++ vues/footer.php
@@ -2,8 +2,63 @@
     <div class="container-fluid" style="background-color: white">
         <div class="row">
             <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-12 col-lg-12>">
-                <div class="display-1 text-center funsized" style="margin-top: 5%;margin-bottom: 5%">
-                    N.M
+                <div class="row">
+
+                    <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-4 col-lg-4">
+
+                        <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-12 col-lg-12">
+                            <div class="row">
+
+                                <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-6 col-lg-6">
+
+                                    <div style="margin-top: 20%;">
+                                     <a href="https://www.linkedin.com/in/maxence-nicole-1a2a71173"><img style="width: 40%" src="Public/img/linkedin.png" alt= "Profile linkedin" ></a>
+                                    </div>
+                                </div>
+
+                                <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-6 col-lg-6 text-right ">
+
+                                    <div style="margin-top: 42%;">
+                                     <a target="_blank" href="https://www.linkedin.com/in/maxence-nicole-1a2a71173"><img style="width: 40%" src="Public/img/linkedin.png" alt= "Profile linkedin" ></a>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-4 col-lg-4>">
+                        <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-12 col-lg-12">
+
+                        <div class="display-1 text-center funsized" style="margin-top: 15%;margin-bottom: 15%">
+                            N.M
+                        </div>
+
+                        </div>
+
+                    </div>
+
+                    <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-4 col-lg-4 text-center">
+
+                        <div class="row">
+
+                            <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-6 col-lg-6 text-left">
+
+                                <div style="margin-top: 40%;">
+                                  <a target="_blank" href="https://github.com/maxence4283"><img style="width: 40%" src="Public/img/github.png" alt= "Profile github" ></a>
+                                </div>
+
+                            </div>
+
+                            <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-6 col-lg-6">
+
+                                <div style="margin-top: 20%;">
+                                  <a href="https://github.com/maxence4283"><img style="width: 40%" src="Public/img/github.png" alt= "Profile github" ></a>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
