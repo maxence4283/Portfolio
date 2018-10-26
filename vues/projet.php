c21d400839c0f181de83d1bd90cf78439444a17c
maxence4283 <maxencen.contact@gmail.com>
2018-10-26 10:54:55 +0200

    Ajout d'une nouvelle photo sur le carousel + petite modif du design des card

--- vues/projet.php
+++ vues/projet.php
@@ -6,11 +6,11 @@
               <p class="display-4 funsized" style="padding-top: 1.5%;margin-bottom: -0.4%">
                   Mes Projets
               </p>
-              <div style="margin-bottom: -2%">
+              <div>
                 <small class="text-muted">(ici sont présentés seulement mes projets principaux et concret)</small>
               </div>
 
-              <hr class="my-5">
+              <hr class="my-4">
 
           </div>
 
@@ -31,14 +31,13 @@
                   <img id="arcenciel" class="card-img-top" src="Public/img/arcenciel.jpg" alt="Les enfants d'arc en ciel">
 
                   <div class="card-body" style="background-color: #b7bdbf">
-                      <h5 class="card-title caviardreams" style="font-size: 1.4rem; margin-bottom: -5%">Les enfants d'arc en ciel</h5>
+                      <h5 class="caviardreams" style="">Les enfants d'arc en ciel</h5>
 
-                      <hr class="my-5" style="width:30%">
+                        <hr class="my-4">
 
                       <p class="card-text champagne" style="margin-top: -5%">Voici un site internet que j'ai réalisé à partir d'une page blanche pour une association
                       dans le cadre de mon stage de première année. La maintenance, le lien avec l'hébérgeur et l'evolution du
                       site internet est géré par moi-même.</p>
-                      <a href="https://www.lesenfantsdarcenciel.ovh/" class="btn btn-outline-secondary btn-lg btn-block sketch" target="_blank" >ACCEDER AU SITE</a>
                   </div>
               </div>
 
@@ -51,35 +50,33 @@
                   <li data-target="#carouselprojet" data-slide-to="1"></li>
                   <li data-target="#carouselprojet" data-slide-to="2"></li>
                   <li data-target="#carouselprojet" data-slide-to="3"></li>
+                  <li data-target="#carouselprojet" data-slide-to="4"></li>
+
                 </ol>
                 <div class="carousel-inner">
                   <div class="carousel-item active">
-                    <img class="d-block w-100" src="Public/img/carousel/lecarrefour.jpg" alt="Le carrefour">
-                    <div class="carousel-caption d-none d-md-block">
-                      <h5>Projet "Le Carrefour"</h5>
-                      <p>Non responsive</p>
-                    </div>
-                  </div>
-                  <div class="carousel-item">
                     <img class="d-block w-100" src="Public/img/carousel/arcenciel.jpg" alt="Arc en ciel">
                     <div class="carousel-caption d-none d-md-block">
-                      <h5>Projet "Arc En Ciel"</h5>
-                      <p>Version ordinateur</p>
+                      <a href="https://www.lesenfantsdarcenciel.ovh/" class="btn btn-dark btn-lg btn-block sketch" target="_blank" >ACCEDER AU SITE</a>
                     </div>
                   </div>
                   <div class="carousel-item">
                     <img class="d-block w-100" src="Public/img/carousel/arcencielmobil.png" alt="Arc en ciel mobile">
                      <div class="carousel-caption d-none d-md-block">
-                        <h5>Projet "Arc En Ciel"</h5>
-                        <p>Version mobile</p>
+                        <a href="https://www.lesenfantsdarcenciel.ovh/" class="btn btn-dark btn-lg btn-block sketch" target="_blank" >ACCEDER AU SITE</a>
                       </div>
                   </div>
                   <div class="carousel-item">
                     <img class="d-block w-100" src="Public/img/carousel/arcencielteblette.png" alt="Arc en ciel Tablette">
                     <div class="carousel-caption d-none d-md-block">
-                      <h5>Projet "Arc En Ciel"</h5>
-                      <p>Version tablette</p>
+                      <a href="https://www.lesenfantsdarcenciel.ovh/" class="btn btn-dark btn-lg btn-block sketch" target="_blank" >ACCEDER AU SITE</a>
                     </div>
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="Public/img/carousel/lecarrefour.jpg" alt="Le carrefour">
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="Public/img/carousel/lecarrefour2.jpg" alt="Le carrefour2">
                   </div>
                 </div>
                 <a class="carousel-control-prev" href="#carouselprojet" role="button" data-slide="prev">
@@ -105,9 +102,9 @@
 
 
                   <div class="card-body" style="background-color: #b7bdbf">
-                      <h5 class="card-title caviardreams" style="font-size: 1.4rem; margin-bottom: -5%">Restaurant Le Carrefour</h5>
+                      <h5 class="caviardreams" style="">Restaurant Le Carrefour</h5>
 
-                      <hr class="my-5" style="width:30%">
+                      <hr class="my-4">
 
                       <p class="card-text champagne" style="margin-top: -5%">Site réalisé avec deux collègues de classe dans le cadre de mon épreuve de bac durant ma terminal STMG SIG, le restaurant n'ayant pas souhaité le mettre en ligne je reste a disposition pour une meilleure visualisation du projet.</p>
                   </div>
