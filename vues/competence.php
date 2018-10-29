5186f26e8ad4ee6d18a9398df4b3983061dafc0d
maxence4283 <maxencen.contact@gmail.com>
2018-10-29 12:02:02 +0100

    Refonte total du design de la partie compétence.

--- vues/competence.php
+++ vues/competence.php
@@ -1,7 +1,7 @@
 <div id="Competence">
   <div class="container-fluid">
     <div class="row">
-        <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-12 col-lg-12" style="background-color: #B7BDBF">
+        <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-12 col-lg-12 pb-4" style="background-color: #B7BDBF">
 
             <p class="text-center display-3 funsized" style="margin-bottom: -2%;padding-top: 1.5%;">
                 competence
@@ -10,98 +10,75 @@
             <hr class="my-5">
 
                 <div class="row" style="margin-top: -1%">
-                    <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-6 col-lg-6 border-right border-secondary">
 
-                        <div class="shadow-lg p-3 mb-5 rounded" style="background-color: #dfe6e9; margin-right: 3%; margin-left: 3%">
+                    <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-6 col-lg-6 champagne font-weight-bold ml-4">
 
-                            <!-- partie lg-->
-                            <p class="champagne d-none d-xl-block" style="font-size: 1.2vw; margin-left: 2%;margin-right: 2%">
-                                Mes compétences acquises lors de mon cursus scolaires et personnelles sont nombreuses :<br><br>
-                                Je suis passionné par les bases de données et tout ce qui les entoures et nottement le langage SQL que je maîtrise pratiquement à 100% seule quelque notions me sont a apprendre. <br><br>
-                                En ce qui concerne le HTML je n'est absolument aucun soucis et pour le CSS de nombreuses compétences me sont acquise nottement avec le framework BOOTSTRAP que je pratique beaucoup.<br><br>
-                                Pour ce qui est du PHP je le pratique assez régulièrement mais étant donnée la richesse de ce langage je ne le maîtrise pas a 100%, de nombreuse bases du langages sont acquise.<br>
-                                La programmation orientée objet et le modèle MVC sont partiellement acquis et en cours d'acquisition.<br><br>
-                                Quant au framework Laravel je m'y interresse activement et suit de nombreux tutoriels le concernant. <br><br>
-                                JAVA à été vue en première année avec la pratique du JAVA de base et du JAVA en programmation orientée objet.
-                            </p>
+                        <div class="shadow-xs pt-2 mb-2" style="background-color:  #b7bdbf;">
 
-                            <!-- partie xl-->
-                            <p class="champagne d-none d-lg-block d-xl-none" style="font-size: 1.2rem; margin-left: 2%;margin-right: 2%">
-                                Mes compétences acquises lors de mon cursus scolaires et personnelles sont nombreuses :<br><br>
-                                Je suis passionné par les bases de données et tout ce qui les entoures et nottement le langage SQL que je maîtrise pratiquement à 100% seule quelque notions me sont a apprendre. <br><br>
-                                En ce qui concerne le HTML je n'est absolument aucun soucis et pour le CSS de nombreuses compétences me sont acquise nottement avec le framework BOOTSTRAP que je pratique beaucoup.<br><br>
-                                Pour ce qui est du PHP je le pratique assez régulièrement mais étant donnée la richesse de ce langage je ne le maîtrise pas a 100%, de nombreuse bases du langages sont acquise.<br>
-                                La programmation orientée objet et le modèle MVC sont partiellement acquis et en cours d'acquisition.<br><br>
-                                Quant au framework Laravel je m'y interresse activement et suit de nombreux tutoriels le concernant. <br><br>
-                                JAVA à été vue en première année avec la pratique du JAVA de base et du JAVA en programmation orientée objet.
-                            </p>
+                            <div class="row">
+                                <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-6 col-lg-6 p-2" style="background-color: #555b61; color:white">
+                                    <p class="text-center">SQL</p>
+                                   <p>Je suis passionné par les bases de données et tout ce qui les entoures et nottement le langage SQL que je maîtrise pratiquement à 100% seule quelque notions me sont a apprendre.</p>
+                                </div>
+                                <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-6 col-lg-6 p-2" style="background-color: white;">
+                                    <p class="text-center">HTML/CSS (BOOTSTRAP)</p>
+                                    <p>En ce qui concerne le HTML je n'est absolument aucun soucis et pour le CSS de nombreuses compétences me sont acquise nottement avec le framework BOOTSTRAP que je pratique beaucoup.</p>
+                                </div>
 
-                            <!-- partie md-->
-                            <p class="champagne d-none d-md-block d-lg-none" style="font-size: 1.2rem; margin-left: 2%;margin-right: 2%">
-                                Mes compétences acquises lors de mon cursus scolaires et personnelles sont nombreuses :<br><br>
-                                Je suis passionné par les bases de données et tout ce qui les entoures et nottement le langage SQL que je maîtrise pratiquement à 100% seule quelque notions me sont a apprendre. <br><br>
-                                En ce qui concerne le HTML je n'est absolument aucun soucis et pour le CSS de nombreuses compétences me sont acquise nottement avec le framework BOOTSTRAP que je pratique beaucoup.<br><br>
-                                Pour ce qui est du PHP je le pratique assez régulièrement mais étant donnée la richesse de ce langage je ne le maîtrise pas a 100%, de nombreuse bases du langages sont acquise.<br>
-                                La programmation orientée objet et le modèle MVC sont partiellement acquis et en cours d'acquisition.<br><br>
-                                Quant au framework Laravel je m'y interresse activement et suit de nombreux tutoriels le concernant. <br><br>
-                                JAVA à été vue en première année avec la pratique du JAVA de base et du JAVA en programmation orientée objet.
-                            </p>
+                                <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-6 col-lg-6 p-2" style="background-color: white;">
+                                    <p class="text-center">PHP</p>
+                                    <p>Pour ce qui est du PHP je le pratique assez régulièrement mais étant donnée la richesse de ce langage je ne le maîtrise pas a 100%, de nombreuse bases du langages sont acquise.
+                                    La programmation orientée objet et le modèle MVC sont partiellement acquis et en cours d'acquisition.</p>
+                                </div>
+                                <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-6 col-lg-6 p-2" style="background-color: #555b61; color:white">
+                                    <p class="text-center">LARAVEL</p>
+                                    <p>Quant au framework Laravel je m'y interresse activement et suit de nombreux tutoriels le concernant.</p>
+                                </div>
 
-                            <!-- partie sm-->
-                            <p class="champagne d-none d-sm-block d-md-none" style="font-size: 1.2rem; margin-left: 2%;margin-right: 2%">
-                                Mes compétences acquises lors de mon cursus scolaires et personnelles sont nombreuses :<br><br>
-                                Je suis passionné par les bases de données et tout ce qui les entoures et nottement le langage SQL que je maîtrise pratiquement à 100% seule quelque notions me sont a apprendre. <br><br>
-                                En ce qui concerne le HTML je n'est absolument aucun soucis et pour le CSS de nombreuses compétences me sont acquise nottement avec le framework BOOTSTRAP que je pratique beaucoup.<br><br>
-                                Pour ce qui est du PHP je le pratique assez régulièrement mais étant donnée la richesse de ce langage je ne le maîtrise pas a 100%, de nombreuse bases du langages sont acquise.<br>
-                                La programmation orientée objet et le modèle MVC sont partiellement acquis et en cours d'acquisition.<br><br>
-                                Quant au framework Laravel je m'y interresse activement et suit de nombreux tutoriels le concernant. <br><br>
-                                JAVA à été vue en première année avec la pratique du JAVA de base et du JAVA en programmation orientée objet.
-                            </p>
-
-                            <!-- partie xs-->
-                            <p class="champagne d-block d-sm-none" style="font-size: 1.2rem; margin-left: 2%;margin-right: 2%">
-                                Mes compétences acquises lors de mon cursus scolaires et personnelles sont nombreuses :<br><br>
-                                Je suis passionné par les bases de données et tout ce qui les entoures et nottement le langage SQL que je maîtrise pratiquement à 100% seule quelque notions me sont a apprendre. <br><br>
-                                En ce qui concerne le HTML je n'est absolument aucun soucis et pour le CSS de nombreuses compétences me sont acquise nottement avec le framework BOOTSTRAP que je pratique beaucoup.<br><br>
-                                Pour ce qui est du PHP je le pratique assez régulièrement mais étant donnée la richesse de ce langage je ne le maîtrise pas a 100%, de nombreuse bases du langages sont acquise.<br>
-                                La programmation orientée objet et le modèle MVC sont partiellement acquis et en cours d'acquisition.<br><br>
-                                Quant au framework Laravel je m'y interresse activement et suit de nombreux tutoriels le concernant. <br><br>
-                                JAVA à été vue en première année avec la pratique du JAVA de base et du JAVA en programmation orientée objet.
-                            </p>
-
+                                <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-6 col-lg-6 p-2" style="background-color: #555b61; color:white">
+                                    <p class="text-center">JAVA</p>
+                                    <p>JAVA à été vue en première année avec la pratique du JAVA de base et du JAVA en programmation orientée objet.</p>
+                                </div>
+                                <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-6 col-lg-6 p-2" style="background-color: white;">
+                                    <p class="text-center">C++</p>
+                                    <p>Je n'ai pas encore eu le temp de m'y interresser mais cela ne serait tarder.</p>
+                                </div>
+                            </div>
                         </div>
+                          
                     </div>
 
-                    <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-6 col-lg-6 caviardreams">
 
-                        <p class="lead">SQL</p>
-                            <div class="progress">
-                                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: 90%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+                    <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-5 col-lg-5 champagne font-weight-bold ml-5">
+
+                        <p style="font-size:1.5vw">SQL</p>
+                            <div class="progress mb-3">
+                                <div class="progress-bar" role="progressbar" style="width: 90%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
                             </div><br>
 
-                        <p class="lead">HTML/CSS (BOOSTRAP)</p>
-                            <div class="progress">
-                                <div class="progress-bar2 progress-bar-striped progress-bar-animated" role="progressbar" style="width: 80%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+                        <p style="font-size:1.5vw">HTML/CSS (BOOSTRAP)</p>
+                            <div class="progress mb-3">
+                                <div class="progress-bar2" role="progressbar" style="width: 80%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
                             </div><br>
 
-                        <p class="lead">PHP</p>
-                            <div class="progress">
-                                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: 70%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+                        <p style="font-size:1.5vw">PHP</p>
+                            <div class="progress mb-3">
+                                <div class="progress-bar" role="progressbar" style="width: 70%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
                             </div><br>
 
-                        <p class="lead">LARAVEL</p>
-                            <div class="progress">
-                                <div class="progress-bar2 progress-bar-striped progress-bar-animated" role="progressbar" style="width: 45%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+                        <p style="font-size:1.5vw">LARAVEL</p>
+                            <div class="progress mb-3">
+                                <div class="progress-bar2" role="progressbar" style="width: 45%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
                             </div><br>
 
-                        <p class="lead">JAVA</p>
-                            <div class="progress">
-                                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: 40%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+                        <p style="font-size:1.5vw">JAVA</p>
+                            <div class="progress mb-3">
+                                <div class="progress-bar" role="progressbar" style="width: 40%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
                             </div><br>
 
-                        <p class="lead">C++</p>
+                        <p style="font-size:1.5vw">C++</p>
                             <div class="progress">
-                                <div class="progress-bar2 progress-bar-striped progress-bar-animated" role="progressbar" style="width: 2%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+                                <div class="progress-bar2" role="progressbar" style="width: 10%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
                             </div><br>
                     </div>
                 </div>
