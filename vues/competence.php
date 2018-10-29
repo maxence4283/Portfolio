56b7cfe63e13b407d4c330a002ea92fd986edc2a
maxence4283 <maxencen.contact@gmail.com>
2018-10-29 19:04:05 +0100

    Avancement de la mise en responsive de la partie compétence

--- vues/competence.php
+++ vues/competence.php
@@ -11,72 +11,69 @@
 
                 <div class="row" style="margin-top: -1%">
 
-                    <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-6 col-lg-6 champagne font-weight-bold ml-4">
-
-                        <div class="shadow-xs pt-2 mb-2" style="background-color:  #b7bdbf;">
+                    <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-6 col-lg-6 champagne font-weight-bold pl-4 pr-4 mb-3">
 
                             <div class="row">
-                                <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-6 col-lg-6 p-2" style="background-color: #555b61; color:white">
+                                <div class="col-12 col-xs-12 col-sm-12 col-md-6 col-xl-6 col-lg-6 p-2" style="background-color: #555b61; color:white">
                                     <p class="text-center">SQL</p>
                                    <p>Je suis passionné par les bases de données et tout ce qui les entoures et nottement le langage SQL que je maîtrise pratiquement à 100% seule quelque notions me sont a apprendre.</p>
                                 </div>
-                                <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-6 col-lg-6 p-2" style="background-color: white;">
+                                <div class="col-12 col-xs-12 col-sm-12 col-md-6 col-xl-6 col-lg-6 p-2" style="background-color: white;">
                                     <p class="text-center">HTML/CSS (BOOTSTRAP)</p>
                                     <p>En ce qui concerne le HTML je n'est absolument aucun soucis et pour le CSS de nombreuses compétences me sont acquise nottement avec le framework BOOTSTRAP que je pratique beaucoup.</p>
                                 </div>
 
-                                <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-6 col-lg-6 p-2" style="background-color: white;">
+                                <div class="col-12 col-xs-12 col-sm-12 col-md-6 col-xl-6 col-lg-6 p-2" style="background-color: white;">
                                     <p class="text-center">PHP</p>
                                     <p>Pour ce qui est du PHP je le pratique assez régulièrement mais étant donnée la richesse de ce langage je ne le maîtrise pas a 100%, de nombreuse bases du langages sont acquise.
                                     La programmation orientée objet et le modèle MVC sont partiellement acquis et en cours d'acquisition.</p>
                                 </div>
-                                <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-6 col-lg-6 p-2" style="background-color: #555b61; color:white">
+                                <div class="col-12 col-xs-12 col-sm-12 col-md-6 col-xl-6 col-lg-6 p-2" style="background-color: #555b61; color:white">
                                     <p class="text-center">LARAVEL</p>
                                     <p>Quant au framework Laravel je m'y interresse activement et suit de nombreux tutoriels le concernant.</p>
                                 </div>
 
-                                <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-6 col-lg-6 p-2" style="background-color: #555b61; color:white">
+                                <div class="col-12 col-xs-12 col-sm-12 col-md-6 col-xl-6 col-lg-6 p-2" style="background-color: #555b61; color:white">
                                     <p class="text-center">JAVA</p>
                                     <p>JAVA à été vue en première année avec la pratique du JAVA de base et du JAVA en programmation orientée objet.</p>
                                 </div>
-                                <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-6 col-lg-6 p-2" style="background-color: white;">
+                                <div class="col-12 col-xs-12 col-sm-12 col-md-6 col-xl-6 col-lg-6 p-2" style="background-color: white;">
                                     <p class="text-center">C++</p>
                                     <p>Je n'ai pas encore eu le temp de m'y interresser mais cela ne serait tarder.</p>
                                 </div>
                             </div>
-                        </div>
                           
                     </div>
 
 
-                    <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-5 col-lg-5 champagne font-weight-bold ml-5">
+                    <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-6 col-lg-6 champagne font-weight-bold pl-4 pr-4">
 
-                        <p style="font-size:1.5vw">SQL</p>
+                        <p style="font-size:1.4rem">SQL</p>
                             <div class="progress mb-3">
                                 <div class="progress-bar" role="progressbar" style="width: 90%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
                             </div><br>
 
-                        <p style="font-size:1.5vw">HTML/CSS (BOOSTRAP)</p>
+                        <p style="font-size:1.4rem">HTML/CSS (BOOSTRAP)</p>
                             <div class="progress mb-3">
                                 <div class="progress-bar2" role="progressbar" style="width: 80%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
                             </div><br>
 
-                        <p style="font-size:1.5vw">PHP</p>
+                        <p style="font-size:1.4rem">PHP</p>
                             <div class="progress mb-3">
                                 <div class="progress-bar" role="progressbar" style="width: 70%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
                             </div><br>
 
-                        <p style="font-size:1.5vw">LARAVEL</p>
+                        <p style="font-size:1.4rem">LARAVEL</p>
                             <div class="progress mb-3">
                                 <div class="progress-bar2" role="progressbar" style="width: 45%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
                             </div><br>
 
-                        <p style="font-size:1.5vw">JAVA</p>
+                        <p style="font-size:1.4rem">JAVA</p>
                             <div class="progress mb-3">
                                 <div class="progress-bar" role="progressbar" style="width: 40%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
                             </div><br>
 
-                        <p style="font-size:1.5vw">C++</p>
+                        <p style="font-size:1.4rem">C++</p>
                             <div class="progress">
                                 <div class="progress-bar2" role="progressbar" style="width: 10%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
                             </div><br>
