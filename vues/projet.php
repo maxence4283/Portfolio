a892c2f83eea0ae5bfefa8bc2b2eca4eb6bac46b
maxence4283 <maxencen.contact@gmail.com>
2018-10-30 10:40:54 +0100

    Design de la partie projet modifié

--- vues/projet.php
+++ vues/projet.php
@@ -46,25 +46,23 @@
                       <div id="caption"></div>
                     </div>
 
-              <div class="card shadow-lg p-2 mb-4 bg-projet rounded" style="width: 100%;">
+              <!-- <div class="card shadow-lg p-0 mb-4 rounded " style="width: 100%; background-color: #555b61"> -->
 
 
                   <img id="arcenciel" class="card-img-top" src="Public/img/arcenciel.jpg" alt="Les enfants d'arc en ciel">
 
-                  <div class="card-body" style="background-color: #b7bdbf">
-                      <h5 class="caviardreams" style="">Les enfants d'arc en ciel</h5>
+                  <div class="card-body" style="background-color: white">
+                      <h4 class="champagne font-weight-bold">Les enfants d'arc en ciel</h4><br>
 
-                        <hr class="my-4">
-
-                      <p class="card-text champagne" style="margin-top: -5%">Voici un site internet que j'ai réalisé à partir d'une page blanche pour une association
+                      <p class="card-text champagne font-weight-bold">Voici un site internet que j'ai réalisé à partir d'une page blanche pour une association
                       dans le cadre de mon stage de première année. La maintenance, le lien avec l'hébérgeur et l'evolution du
                       site internet est géré par moi-même.</p>
                   </div>
-              </div>
+              <!-- </div> -->
 
           </div>
-          <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 text-center">
-            <div class="card shadow-lg p-2 mb-4 bg-projet rounded" style="width: 100%;">
+          <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 text-center mb-4">
+            <!-- <div class="card shadow-lg p-2 mb-4 bg-projet rounded" style="width: 100%;"> -->
               <div id="carouselprojet" class="carousel slide" data-ride="carousel">
                 <ol class="carousel-indicators">
                   <li data-target="#carouselprojet" data-slide-to="0" class="active"></li>
@@ -78,19 +76,19 @@
                   <div class="carousel-item active">
                     <img class="d-block w-100" src="Public/img/carousel/arcenciel.jpg" alt="Arc en ciel">
                     <div class="carousel-caption d-none d-md-block">
-                      <a href="https://www.lesenfantsdarcenciel.ovh/" class="btn btn-dark btn-lg btn-block sketch" target="_blank" >ACCEDER AU SITE</a>
+                      <a href="https://www.lesenfantsdarcenciel.ovh/" class="btn btn-light btn-lg btn-block sketch" target="_blank" >ACCEDER AU SITE</a>
                     </div>
                   </div>
                   <div class="carousel-item">
                     <img class="d-block w-100" src="Public/img/carousel/arcencielmobil.png" alt="Arc en ciel mobile">
                      <div class="carousel-caption d-none d-md-block">
-                        <a href="https://www.lesenfantsdarcenciel.ovh/" class="btn btn-dark btn-lg btn-block sketch" target="_blank" >ACCEDER AU SITE</a>
+                        <a href="https://www.lesenfantsdarcenciel.ovh/" class="btn btn-light btn-lg sketch" target="_blank" >ACCEDER AU SITE</a>
                       </div>
                   </div>
                   <div class="carousel-item">
                     <img class="d-block w-100" src="Public/img/carousel/arcencielteblette.png" alt="Arc en ciel Tablette">
                     <div class="carousel-caption d-none d-md-block">
-                      <a href="https://www.lesenfantsdarcenciel.ovh/" class="btn btn-dark btn-lg btn-block sketch" target="_blank" >ACCEDER AU SITE</a>
+                      <a href="https://www.lesenfantsdarcenciel.ovh/" class="btn btn-light btn-lg sketch" target="_blank" >ACCEDER AU SITE</a>
                     </div>
                   </div>
                   <div class="carousel-item">
@@ -110,26 +108,24 @@
                 </a>
               </div>
 
-            </div>
+            <!-- </div> -->
               
           </div>
 
           <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-lg-3 col-xl-3 text-center">
 
-              <div class="card shadow-lg p-2 mb-4 bg-projet rounded" style="width: 100%;">
+              <!-- <div class="card shadow-lg p-0 mb-4 bg-projet rounded" style="width: 100%; background-color: white"> -->
 
 
                   <img id="carrefour" class="card-img-top" src="Public/img/lecarrefour.jpg" alt="Restaurant Le Carrefour">
 
 
-                  <div class="card-body" style="background-color: #b7bdbf">
-                      <h5 class="caviardreams" style="">Restaurant Le Carrefour</h5>
+                  <div class="card-body" style="background-color: #555b61">
+                      <h4 class="champagne font-weight-bold" style=" color:white">Restaurant Le Carrefour</h4><br>
 
-                      <hr class="my-4">
-
-                      <p class="card-text champagne" style="margin-top: -5%">Site réalisé avec deux collègues de classe dans le cadre de mon épreuve de bac durant ma terminal STMG SIG, le restaurant n'ayant pas souhaité le mettre en ligne je reste a disposition pour une meilleure visualisation du projet.</p>
+                      <p class="card-text champagne font-weight-bold" style="color: white">Site réalisé avec deux collègues de classe dans le cadre de mon épreuve de bac durant ma terminal STMG SIG, le restaurant n'ayant pas souhaité le mettre en ligne je reste a disposition pour une meilleure visualisation du projet.</p>
                   </div>
-              </div>
+              <!-- </div> -->
 
           </div>
       </div>
