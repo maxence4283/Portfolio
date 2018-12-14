87528c6d2ea0ef6fc4c9efa6cb6ed203b2265daf
maxence4283 <maxencen.contact@gmail.com>
2018-12-14 11:27:11 +0100

    Réctification disposition compétence.

--- vues/competence.php
+++ vues/competence.php
@@ -8,131 +8,143 @@
             </p>
 
         </div>
-        <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-12 col-lg-12 pb-4 pt-4">
-                <div class="row  mb-5">
+                <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-12 col-lg-12 champagne font-weight-bold mb-1 mt-3 pl-4 pr-4 textecompetence">
 
-                    <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-6 col-lg-6 champagne font-weight-bold pl-4 pr-4 mb-3 textecompetence">
-
-                            <div class="row">
-                                <div class="col-12 col-xs-12 col-sm-12 col-md-6 col-xl-6 col-lg-6 p-2" style="background-color: #555b61; color:white">
+                      <div class="row">
+                        <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-4 col-lg-4">
+                                <div class="p-2" style="background-color: #555b61; color:white">
                                     <p class="text-center">SQL</p>
                                    <p style="margin-bottom: 7vw">Je suis passionné par les bases de données dans toutes leurs globalités et notamment le langage SQL que je maîtrise pratiquement à 100% seule quelque notions me sont à apprendre.</p>
                                 </div>
-                                <div class="col-12 col-xs-12 col-sm-12 col-md-6 col-xl-6 col-lg-6 p-2" style="background-color: white;">
+                                <div class="p-2" style="background-color: white;">
                                     <p class="text-center">HTML/CSS (BOOTSTRAP)</p>
-                                    <p>En ce qui concerne le HTML je le maîtrise en grande partie et pour le CSS de nombreuses compétences me sont acquises notamment avec le framework BOOTSTRAP que je pratique beaucoup.</p>
+                                    <p style="margin-bottom: 10vw">En ce qui concerne le HTML je le maîtrise en grande partie et pour le CSS de nombreuses compétences me sont acquises notamment avec le framework BOOTSTRAP que je pratique beaucoup.</p>
                                 </div>
-
-                                <div class="col-12 col-xs-12 col-sm-12 col-md-6 col-xl-6 col-lg-6 p-2" style="background-color: white;">
+                        </div>
+                        <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-4 col-lg-4">
+                                <div class="p-2" style="background-color: white;">
                                     <p class="text-center">PHP</p>
-                                    <p style="margin-bottom: 5vw">Pour ce qui est du PHP je le pratique assez régulièrement mais étant donné la richesse de ce langage je ne le maîtrise pas à 100%, de nombreuse bases du langages sont acquises.
+                                    <p style="margin-bottom: 3.75vw">Pour ce qui est du PHP je le pratique assez régulièrement mais étant donné la richesse de ce langage je ne le maîtrise pas à 100%, de nombreuse bases du langages sont acquises.
                                     La programmation orientée objet et le modèle MVC sont partiellement acquis, ce site est développé en MVC.</p>
                                 </div>
-                                <div class="col-12 col-xs-12 col-sm-12 col-md-6 col-xl-6 col-lg-6 p-2" style="background-color: #555b61; color:white">
+                                <div class="p-2" style="background-color: #555b61; color:white">
                                     <p class="text-center">LARAVEL</p>
-                                    <p>Quant au framework Laravel je m'y intéresse activement et suis de nombreux tutoriels le concernant.</p>
+                                    <p style="margin-bottom: 13vw">Quant au framework Laravel je m'y intéresse activement et suis de nombreux tutoriels le concernant.</p>
                                 </div>
-
-                                <div class="col-12 col-xs-12 col-sm-12 col-md-6 col-xl-6 col-lg-6 p-2" style="background-color: #555b61; color:white">
+                        </div>
+                        <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-4 col-lg-4">
+                                <div class="p-2" style="background-color: #555b61; color:white">
                                     <p class="text-center">JAVA</p>
-                                    <p style="margin-bottom: 15vw">JAVA à été vue en première année avec la pratique du JAVA de base et du JAVA en programmation orientée objet.</p>
+                                    <p style="margin-bottom: 10vw">JAVA à été vue en première année avec la pratique du JAVA de base et du JAVA en programmation orientée objet.</p>
                                 </div>
-                                <div class="col-12 col-xs-12 col-sm-12 col-md-6 col-xl-6 col-lg-6 p-2" style="background-color: white;">
+                                <div class="p-2" style="background-color: white;">
                                     <p class="text-center">GIT/GITHUB</p>
-                                    <p>J'ai beaucoup utilisé GIT avec l'outil en ligne GITHUB afin de gérer le versionning de mon code, notamment pour ce site.</p>
+                                    <p style="margin-bottom: 13vw">J'ai beaucoup utilisé GIT avec l'outil en ligne GITHUB afin de gérer le versionning de mon code, notamment pour ce site.</p>
                                 </div>
-                            </div>
-                          
-                    </div>
-
-                    <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-6 col-lg-6 dispositioncompetence">
-
-                        <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-12 col-lg-12 mb-4 rounded pt-2 pr-2 pl-2 shadow-lg" style="background-color:white">
-
-                            <p class="textecompetence2" style="color:#555b61">
-                                <i class="fas fa-database iconecompetence"></i> SQL</p>
-                                <div class="progresscompetence">
-                                    <div class="progress-barcompetence" role="progressbar" style="width: 90%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
-                                </div><br>
-
                         </div>
-
-                        <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-12 col-lg-12 mb-4 rounded pt-2 pr-2 pl-2 shadow-lg" style="background-color:white">
-
-                            <p class="textecompetence2" style="color:#555b61">
-                                <i class="fab fa-html5 iconecompetence"></i> HTML 
-                                <i class="fab fa-css3-alt ml-4 iconecompetence"></i> CSS3 (BOOSTRAP)
-
-                            </p>
-                                <div class="progresscompetence">
-                                    <div class="progress-barcompetence" role="progressbar" style="width: 80%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
-                                </div><br>
-
-                        </div>
-
-                        <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-12 col-lg-12 mb-4 rounded pt-2 pr-2 pl-2 shadow-lg" style="background-color:white">
-
-                            <p class="textecompetence2" style="color:#555b61">
-
-                                <i class="fab fa-php iconecompetence"></i> 
-
-                                    PHP
-                            </p>
-                                <div class="progresscompetence">
-                                    <div class="progress-barcompetence" role="progressbar" style="width: 70%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
-                                </div><br>
-
-                        </div>
-
-                        <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-12 col-lg-12 mb-4 rounded pt-2 pr-2 pl-2 shadow-lg" style="background-color:white">
-
-                            <p class="textecompetence2" style="color:#555b61">
-
-                                <i class="fab fa-git iconecompetence"></i> GIT 
-                                <i class="fab fa-github-alt ml-4 iconecompetence"></i> GITHUB
-
-                            </p>
-
-                                <div class="progresscompetence">
-                                    <div class="progress-barcompetence" role="progressbar" style="width: 60%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
-                                </div><br>
-
-                        </div>
-
-                        <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-12 col-lg-12 mb-4 rounded pt-2 pr-2 pl-2 shadow-lg" style="background-color:white">
-
-                            <p class="textecompetence2" style="color:#555b61">
-                                <i class="fab fa-wordpress-simple iconecompetence"></i> WORDPRESS
-
-                            </p>
-                                <div class="progresscompetence">
-                                    <div class="progress-barcompetence" role="progressbar" style="width: 50%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
-                                </div><br>
-
-                        </div>
-
-                        <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-12 col-lg-12 mb-4 rounded pt-2 pr-2 pl-2 shadow-lg" style="background-color:white">
-
-                            <p class="textecompetence2" style="color:#555b61"><i class="fab fa-laravel iconecompetence"></i> LARAVEL</p>
-                                <div class="progresscompetence">
-                                    <div class="progress-barcompetence" role="progressbar" style="width: 45%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
-                                </div><br>
-
-                        </div>
-
-                        <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-12 col-lg-12 mb-4 rounded pt-2 pr-2 pl-2 shadow-lg" style="background-color:white">
-
-                            <p class="textecompetence2" style="color:#555b61"><i class="fab fa-java iconecompetence"></i> JAVA</p>
-                                <div class="progresscompetence">
-                                    <div class="progress-barcompetence" role="progressbar" style="width: 40%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
-                                </div><br>
-
-                        </div>
-
-                    </div>
-
+                     </div>
                 </div>
-        </div>
+
+            <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-12 col-lg-12 mt-4 pl-4 pr-4">
+                <div class="row">
+                    <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-6 col-lg-6 mt-1">
+
+                            <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-12 col-lg-12 mb-4 rounded pt-2 pr-2 pl-2 shadow-lg" style="background-color:white">
+
+                                <p class="textecompetence2" style="color:#555b61">
+                                    <i class="fas fa-database iconecompetence"></i> SQL</p>
+                                    <div class="progresscompetence">
+                                        <div class="progress-barcompetence" role="progressbar" style="width: 90%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div><br>
+
+                            </div>
+
+                            <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-12 col-lg-12 mb-4 rounded pt-2 pr-2 pl-2 shadow-lg" style="background-color:white">
+
+                                <p class="textecompetence2" style="color:#555b61">
+                                    <i class="fab fa-html5 iconecompetence"></i> HTML
+                                </p>
+                                    <div class="progresscompetence">
+                                        <div class="progress-barcompetence" role="progressbar" style="width: 80%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div><br>
+
+                            </div>
+
+                            <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-12 col-lg-12 mb-4 rounded pt-2 pr-2 pl-2 shadow-lg" style="background-color:white">
+
+                                <p class="textecompetence2" style="color:#555b61">
+                                    <i class="fab fa-css3-alt iconecompetence"></i> CSS3 (BOOSTRAP)
+
+                                </p>
+                                    <div class="progresscompetence">
+                                        <div class="progress-barcompetence" role="progressbar" style="width: 75%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div><br>
+
+                            </div>
+
+                            <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-12 col-lg-12 mb-4 rounded pt-2 pr-2 pl-2 shadow-lg" style="background-color:white">
+
+                                <p class="textecompetence2" style="color:#555b61">
+
+                                    <i class="fab fa-php iconecompetence"></i> 
+
+                                        PHP
+                                </p>
+                                    <div class="progresscompetence">
+                                        <div class="progress-barcompetence" role="progressbar" style="width: 70%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div><br>
+
+                            </div>
+
+                    </div>
+                    <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-6 col-lg-6 mt-1">
+
+                            <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-12 col-lg-12 mb-4 rounded pt-2 pr-2 pl-2 shadow-lg" style="background-color:white">
+
+                                <p class="textecompetence2" style="color:#555b61">
+
+                                    <i class="fab fa-git iconecompetence"></i> GIT 
+                                    <i class="fab fa-github-alt ml-4 iconecompetence"></i> GITHUB
+
+                                </p>
+
+                                    <div class="progresscompetence">
+                                        <div class="progress-barcompetence" role="progressbar" style="width: 60%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div><br>
+
+                            </div>
+
+                            <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-12 col-lg-12 mb-4 rounded pt-2 pr-2 pl-2 shadow-lg" style="background-color:white">
+
+                                <p class="textecompetence2" style="color:#555b61">
+                                    <i class="fab fa-wordpress-simple iconecompetence"></i> WORDPRESS
+
+                                </p>
+                                    <div class="progresscompetence">
+                                        <div class="progress-barcompetence" role="progressbar" style="width: 50%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div><br>
+
+                            </div>
+
+                            <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-12 col-lg-12 mb-4 rounded pt-2 pr-2 pl-2 shadow-lg" style="background-color:white">
+
+                                <p class="textecompetence2" style="color:#555b61"><i class="fab fa-laravel iconecompetence"></i> LARAVEL</p>
+                                    <div class="progresscompetence">
+                                        <div class="progress-barcompetence" role="progressbar" style="width: 45%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div><br>
+
+                            </div>
+
+                            <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-xl-12 col-lg-12 mb-4 rounded pt-2 pr-2 pl-2 shadow-lg" style="background-color:white">
+
+                                <p class="textecompetence2" style="color:#555b61"><i class="fab fa-java iconecompetence"></i> JAVA</p>
+                                    <div class="progresscompetence">
+                                        <div class="progress-barcompetence" role="progressbar" style="width: 40%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div><br>
+
+                            </div>
+                </div>
+            </div>
     </div>
   </div>
 </div>
